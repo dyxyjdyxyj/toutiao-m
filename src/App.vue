2c610eaf1c3e3388a0e51a1954d57aa4d8b4e8d6
dongyangxi <41719683@qq.com>
2022-06-19 23:35:30 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <i class="toutiao toutiao-dianzan"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+// 加载vant样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import '@/styles/index.less'
 
 Vue.config.productionTip = false
 
+Vue.use(Vant)
 new Vue({
   router,
   store,
